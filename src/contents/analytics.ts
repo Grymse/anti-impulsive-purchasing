d6356076a8cea198801736f14c5ea80020cb8512
Nicolai Grymer <pronicoxd@gmail.com>
2025-02-11 15:35:31 +0100

    Adding analytics for add-to-cart. Start work on read checkout

--- src/contents/analytics.ts
+++ src/contents/analytics.ts
@@ -1,34 +1,134 @@
-// content.ts
-
 import { defaultConfig } from "~lib/config";
-import { getters } from "~lib/getters";
+import { getters, type ShoppingItem } from "~lib/getters";
 
 export const config = defaultConfig;
 
-
-// Main content script logic
-/* const observer = new MutationObserver((mutations) => {
-  // Apply changes as soon as elements exist
-  if (document.body) {
-    // Apply your DOM modifications here
-    document.body.style.backgroundColor = "lightblue"
-    
-    // Cleanup observer if needed
-    observer.disconnect()
-  }
-})
-
-window.addEventListener("load", () => {
-  // Start observing the document
-  observer.observe(document.documentElement, {
-    childList: true,
-    subtree: true
-  })
-}) */
-
-
 function setup() {
-  getters.getDomainGetters().addToCartButtons();
+  const addToCartButtons = getters.getDomainGetters().addToCartButtons();
+  /* test(); */
+  addToCartButtons.forEach((button) => {
+    button.addEventListener("click", onAddToCartClick);
+  });
+
+  const items = getters.getDomainGetters().getCartItems();
+  console.log(items);
+}
+
+function onAddToCartClick(e: Event) {
+  sendAnalytics('add-to-cart', undefined);
 }
 
 window.addEventListener("load", setup);
+
+type AnalyticsEvent = {
+  type: keyof AnalyticsPayloads;
+  url: string;
+  payload: string;
+  userId: string;
+  sessionId: string;
+  unixTime: number;
+}
+
+type AnalyticsPayloads = {
+  'add-to-cart': undefined;
+  'checkout': ShoppingItem[];
+  'place-order': ShoppingItem[];
+}
+
+function sendAnalytics<T extends keyof AnalyticsPayloads>(type: T, payload: AnalyticsPayloads[T]) {
+  const data: AnalyticsEvent = {
+    type,
+    url: window.location.href,
+    payload: JSON.stringify(payload),
+    userId: getUserId(),
+    sessionId: getSessionId(),
+    unixTime: Date.now()
+  }
+
+  // Send the analytics data to the server
+  console.log("ANALYTICS:",data);
+}
+
+// TODO: Create if none. Reset if expired?
+function getSessionId() {
+  return 'sessionid'
+}
+
+// TODO: Create if none.
+function getUserId(): string {
+  return 'userid'
+}
+
+/* function test() {
+  const button = document.querySelector<HTMLElement>('#add-to-cart-button');
+  const section = button.closest(".a-section");
+  const site = button.closest("#ppd");
+  const buttons : AddButton[] = [];
+
+  buttons.push({
+    button,
+    getItems: () => {
+      const id = site?.querySelector<HTMLElement>('#title')?.innerText;
+      const quantity = section?.querySelector<HTMLElement>('.a-dropdown-prompt')?.innerText;
+      const priceWhole = section?.querySelector<HTMLElement>('.a-offscreen')?.innerText;
+      const currency = section?.querySelector<HTMLElement>('.a-price-symbol')?.innerText;
+      const price = priceWhole?.replace(currency, '');
+
+      return [{
+        quantity: quantity ? parseInt(quantity) : 0,
+        price: price ? parseFloat(price) : 0,
+        currency: currency ? currency : '',
+      }];
+    },
+  });
+
+  const tableButtons = document.querySelectorAll<HTMLElement>('table input[name="submit.addToCart"]');
+
+  tableButtons.forEach((button, index) => {
+    const table = button.closest('table');
+    const pricetr = table.querySelectorAll('tr')[2];
+    buttons.push({
+      button,
+      getItems: () => {
+        const priceWhole = pricetr?.querySelectorAll<HTMLElement>('.a-offscreen')[index]?.innerText;
+        const currency = pricetr?.querySelectorAll<HTMLElement>('.a-price-symbol')[index]?.innerText;
+        const price = priceWhole?.replace(currency, '');
+
+        return [{
+          quantity: 1,
+          price: price ? parseFloat(price) : 0,
+          currency: currency ? currency : '',
+        }];
+      },
+    });
+  });
+
+  const bothToCartButton = document.querySelector<HTMLElement>('div[data-price-totals] input[name="submit.addToCart"]');
+  buttons.push({
+    button: bothToCartButton,
+    getItems: () => {
+      const data = JSON.parse(bothToCartButton.closest('div[data-price-totals]').getAttribute('data-components'));
+      
+      return Object.values(data).map((item: any) => {
+        console.log(item);
+        const currency = item?.price?.currencySymbol;
+        const price = item.price.displayString.replace(currency, '');
+        return {
+          quantity: item.minQuantity,
+          price: price ? parseFloat(price) : 0,
+          currency: currency ? currency : '',
+        };
+      });
+  }});
+
+  buttons.flatMap(button => button.getItems()).forEach(console.log);
+  buttons.forEach(button => {
+    button.button.parentElement.style.backgroundColor = 'red';
+  });
+}
+ */
+
+type AddButton = {
+  button: HTMLElement;
+  getItems: () => ShoppingItem[];
+}
--- src/lib/getters.ts
+++ src/lib/getters.ts
@@ -4,6 +4,13 @@ export type ElementGetters = {
     placeOrderButtons: () => Element[];
     checkoutButtonLabels: () => Element[];
     addToCartButtons: () => Element[];
+    getCartItems: () => ShoppingItem[];
+}
+
+export type ShoppingItem = {
+    quantity: number;
+    price: number;
+    currency: string;
 }
 
 type GetterRegister = {
@@ -41,12 +48,14 @@ export const getters: GetterRegister = {
         return this._getters.get(domain) || {
             checkoutButtons: () => [],
             placeOrderButtons: () => [],
-            checkoutButtonLabels: () => []
+            checkoutButtonLabels: () => [],
+            addToCartButtons: () => [],
+            getCartItems: () => []
         };
     }
 }
 
-getters.register('www.amazon.com',  {
+getters.register('www.amazon.com', {
     checkoutButtons: () => {
       const buttons = document.querySelectorAll('input[name="proceedToRetailCheckout"]');
       return Array.from(buttons);
@@ -57,18 +66,41 @@ getters.register('www.amazon.com',  {
       return Array.from(buttons);
     },
 
-    checkoutButtonLabels: () => {
+    checkoutButtonLabels: () => { 
       const buttons = document.querySelectorAll('div[data-feature-id="proceed-to-checkout-label"], #submitOrderButtonId-announce, #bottomSubmitOrderButtonId-announce');
       return Array.from(buttons);
     },
 
     addToCartButtons: () => {
-      const buttons = document.querySelectorAll('#add-to-cart-button, input[name="submit.addToCart"], .add-to-cart .a-button-input');
+      const buttons = document.querySelectorAll('#add-to-cart-button, [name="submit.addToCart"], .add-to-cart .a-button-input, input[data-asin], div[data-csa-c-action="addToCart"] button');
       buttons.forEach(button => button.parentElement.style.backgroundColor = 'red');
       return Array.from(buttons);
+    },
+
+    getCartItems: () => {
+        const quantities = document.querySelectorAll<HTMLElement>('div[name="sc-quantity"] span[data-a-selector="value"]')
+        const priceSymbol = document.querySelectorAll<HTMLElement>('#sc-active-cart .a-price span.a-price-symbol');
+        const priceWhole = document.querySelectorAll<HTMLElement>('#sc-active-cart .a-price span.a-price-whole');
+        const priceFraction = document.querySelectorAll<HTMLElement>('#sc-active-cart .a-price span.a-price-fraction');
+        const items = [];
+        for (let i = 0; i < priceSymbol.length; i++) {
+            items.push({
+                quantity: parseInt(quantities[i].innerText),
+                price: parseFloat(priceWhole[i].innerText) + parseFloat(priceFraction[i].innerText) / 100,
+                currency: priceSymbol[i].innerText
+            });
+        }
+        /* const elements = document.querySelectorAll<HTMLElement>('span.a-list-item div[data-csa-c-type="item"]');
+        const x = elements.entries().map(([_, element]) => {
+            return {
+                quantity: parseFloat(element.getAttribute('data-quantity')),
+                price: parseFloat(element.getAttribute('data-price')),
+                currency: JSON.parse(element.getAttribute('data-subtotal'))?.subtotal?.code
+            }
+        }).toArray(); */
+        return items;
     }
-}
-);
+});
 
 getters.register('www.zalando.dk', {
     checkoutButtons: () => {
@@ -88,5 +120,9 @@ getters.register('www.zalando.dk', {
 
     addToCartButtons: () => {
         return [];
-      }
+    },
+
+    getCartItems: () => {
+        return [];
+    }
 });
