3283e590a8092f7587112afa01282d871f14e40c
Nicolai Grymer <pronicoxd@gmail.com>
2025-02-18 13:07:40 +0100

    add webpage

---
+++ policy-web/src/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { cn } from './lib/utils'
+
+type Props = {variant?: 'h1' | 'h2' | 'h3', children: React.ReactNode, className?: string}
+
+
+export default function Header({variant = 'h1', children, className}: Props) {
+    switch (variant) {
+        case 'h1':
+        return <h1 className={cn('scroll-m-20 pt-6 text-3xl font-bold tracking-tight text-foreground', className)}>{children}</h1>
+        case 'h2':
+        return <h2 className={cn('pt-6 text-foreground font-heading scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0', className)}>{children}</h2>
+        case 'h3':
+        return <h3 className={cn('text-lg font-bold text-foreground', className)}>{children}</h3>
+    }
+}
---
+++ policy-web/src/Lists.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type UnorederedListProps = {children: React.ReactNode}
+
+export function Ul({children}: UnorederedListProps) {
+  return <ul className="my-6 ml-6 list-disc">{children}</ul>
+}
+
+export function Li({children}: {children: React.ReactNode}) {
+  return <li className="mt-2">{children}</li>
+}
