e187d2605eabe2fa28b8a2011bdce51a734eb0f6
Frederik Rothe <fr@notiva.dk>
2025-02-19 15:06:22 +0100

    button-to-extension-page

--- policy-web/src/App.tsx
+++ policy-web/src/App.tsx
@@ -1,14 +1,14 @@
-import Header from "./Header"
-import Text from './Text';
-import { Li, Ul } from "./Lists"
-import { Link } from './Text';
+import Header from "./Header";
+import Text from "./Text";
+import { Li, Ul } from "./Lists";
+import { Link } from "./Text";
 import { Button } from "./components/ui/button";
 
 export default function App() {
   // Detect the user's system preference for dark mode
   const preferDarkmode = window.matchMedia(
     "(prefers-color-scheme: dark)"
-  ).matches
+  ).matches;
 
   console.log(preferDarkmode);
 
@@ -20,29 +20,36 @@ export default function App() {
         flex flex-col items-center justify-center
         bg-background
         px-8 py-6
-      `}>
+      `}
+    >
       <div className="max-w-2xl space-y-2">
         <div className="flex flex-col items-center justify-center mb-8">
-          <blockquote className='text-primary text-xl italic my-16'>
+          <blockquote className="text-primary text-xl italic my-16">
             “One who is patient glows with an inner radiance.” - Allan Lokos
           </blockquote>
-        {/* Logo Section */}
-        <div className="mb-12 text-center">
-          <img
-              src='/icon.png'
+          {/* Logo Section */}
+          <div className="mb-12 text-center">
+            <img
+              src="/icon.png"
               alt="Extension Logo (On)"
               width={128}
               height={128}
               className="w-full h-full max-w-[200px] animation-breathe"
             />
+          </div>
+          {/* Toggle Button */}
+          <Button
+            className={"w-1/2 mb-4"}
+            onClick={() => {
+              window.location.href =
+                "https://chromewebstore.google.com/detail/less/kcgblchgejkpnemehaojecgbamdiacml";
+            }}
+          >
+            Visit the Extension Page
+          </Button>
         </div>
-        {/* Toggle Button */}
-        <Button className={'w-1/2 mb-4'} onClick={() => {window.alert("Waiting for vetting process")}}>
-          Visit the Extension Page
-        </Button>
-      </div>
-      {/* First Text Block */}
-      
+        {/* First Text Block */}
+
         <Header variant="h1">Research Description</Header>
         <Text>
           Welcome to Less, a browser extension designed to help reduce impulsive
@@ -54,7 +61,6 @@ export default function App() {
           reducing online impulsive buying behaviors, this tool is meant to
           support both your financial well-being and the environment.
         </Text>
-
 
         <Header variant="h2">Supported Shopping Sites</Header>
         <Text>
@@ -83,9 +89,9 @@ export default function App() {
             </Li>
           </Ul>
         </Text>
-      
-      {/* Second Text Block */}
-      
+
+        {/* Second Text Block */}
+
         <Header variant="h1">Analytics</Header>
         <Text>
           The extension is only active on a fixed list of known shopping
@@ -97,28 +103,28 @@ export default function App() {
             <Li>All data is aggregated before any use. </Li>
             <Li>Data is not sold or shared with third parties. </Li>
           </Ul>
-          
-          For full transparency, the extension’s open-source code is available here on our{" "}
+          For full transparency, the extension’s open-source code is available
+          here on our{" "}
           <Link href="https://github.com/Grymse/anti-impulsive-purchasing">
             GitHub
           </Link>
           . In addition, be sure to read our Terms of Service before use.
         </Text>
-      
-      {/* Third Text Block */}
-      <br />
+
+        {/* Third Text Block */}
+        <br />
         <Header variant="h1">Terms of Service</Header>
         <Text>A description of the terms of service</Text>
-        
+
         <Header variant="h2">Introduction</Header>
         <Text>
-          Welcome to Less, a browser extension developed as part of a thesis
-          on reducing impulse purchasing habits. This extension aims to gather
+          Welcome to Less, a browser extension developed as part of a thesis on
+          reducing impulse purchasing habits. This extension aims to gather
           insights into user behavior and help develop strategies to minimize
-          impulse purchases. By installing or using this extension, you agree
-          to the following terms and conditions.
+          impulse purchases. By installing or using this extension, you agree to
+          the following terms and conditions.
         </Text>
-      
+
         <Header variant="h2">Eligibility</Header>
         <Text>
           Use of this extension is intended for individuals aged 18 and older
@@ -128,111 +134,90 @@ export default function App() {
 
         <Header variant="h2">Voluntary Participation</Header>
         <Text>
-          Your use of this extension is entirely voluntary. You may
-          discontinue use at any time without any consequences or obligations.
-          There is no requirement to continue using the extension once
-          installed, and you may disable or remove it at will.
+          Your use of this extension is entirely voluntary. You may discontinue
+          use at any time without any consequences or obligations. There is no
+          requirement to continue using the extension once installed, and you
+          may disable or remove it at will.
         </Text>
-        
+
         <Header variant="h2">Data Collection and Usage</Header>
         <Text>
-          The data collected include information about your browsing activity
-          on the listed websites above, along with details about the amount of
-          purchases, and products individual pricing. All
-          collected data will be anonymized and used solely for academic
-          research purposes related to reducing impulse purchasing behavior.
-          This information may also appear in publications or presentations
-          based on the research.
+          The data collected include information about your browsing activity on
+          the listed websites above, along with details about the amount of
+          purchases, and products individual pricing. All collected data will be
+          anonymized and used solely for academic research purposes related to
+          reducing impulse purchasing behavior. This information may also appear
+          in publications or presentations based on the research.
         </Text>
-      
+
         <Header variant="h2">Confidentiality and Privacy</Header>
         <Text>
           Your privacy is paramount. Any personal data is processed and stored
           securely, in accordance with applicable privacy regulations (e.g.,
-          GDPR, CCPA). Identifiable data, if collected, will not be shared
-          with third parties or used for purposes unrelated to this research.
-          All published research findings will be reported in a manner that
+          GDPR, CCPA). Identifiable data, if collected, will not be shared with
+          third parties or used for purposes unrelated to this research. All
+          published research findings will be reported in a manner that
           safeguards anonymity.
         </Text>
-      
 
         {/* Data Security */}
-        
-          <Header variant="h2">Data Security</Header>
-          <Text>
-            All collected data is stored securely using Supabase
-            database service. Reasonable measures are in place to prevent
-            unauthorized access, loss, or disclosure.
-          </Text>
-        
+
+        <Header variant="h2">Data Security</Header>
+        <Text>
+          All collected data is stored securely using Supabase database service.
+          Reasonable measures are in place to prevent unauthorized access, loss,
+          or disclosure.
+        </Text>
 
         {/* Liability Disclaimer */}
-        
-          <Header variant="h2">Liability Disclaimer</Header>
-          <Text>
-            Although efforts are made to secure your data, Nicolai Grymer and
-            IT-University of Copenhagen are not liable for any unauthorized
-            access or unforeseen data breaches that may occur despite best
-            practices.
-          </Text>
-        
+
+        <Header variant="h2">Liability Disclaimer</Header>
+        <Text>
+          Although efforts are made to secure your data, Nicolai Grymer and
+          IT-University of Copenhagen are not liable for any unauthorized access
+          or unforeseen data breaches that may occur despite best practices.
+        </Text>
 
         {/* Right to Withdraw and Data Removal */}
-        
-          <Header variant="h2">Right to Withdraw and Data Removal</Header>
-          <Text>
-            If, after providing data through the extension, you wish to withdraw
-            your anonymized data from the study, you must make a request within
-            30 days of your usage. After this period, data may be aggregated and
-            fully anonymized, making specific entries unidentifiable and thus
-            impossible to remove. To request data removal, please contact us
-            promptly at the emails listed below.
-          </Text>
-        
+
+        <Header variant="h2">Right to Withdraw and Data Removal</Header>
+        <Text>
+          If, after providing data through the extension, you wish to withdraw
+          your anonymized data from the study, you must make a request within 30
+          days of your usage. After this period, data may be aggregated and
+          fully anonymized, making specific entries unidentifiable and thus
+          impossible to remove. To request data removal, please contact us
+          promptly at the emails listed below.
+        </Text>
 
         {/* Contact Information */}
-        
-          <Header variant="h2">Contact Information</Header>
-          <Text>
-            For any questions or concerns regarding this extension, your
-            participation, or the data collected, please contact:
-          
-            <Ul>
-              <Li>
-                Nicolai Grymer:{" "}
-                <Link
-                  href="mailto:ngry@itu.dk"
-                >
-                  ngry@itu.dk
-                </Link>
-              </Li>
-              <Li>
-                Frederik Rothe:{" "}
-                <Link
-                  href="mailto:frot@itu.dk"
-                >
-                  frot@itu.dk
-                </Link>
-              </Li>
-            </Ul>
-          </Text>
-        
+
+        <Header variant="h2">Contact Information</Header>
+        <Text>
+          For any questions or concerns regarding this extension, your
+          participation, or the data collected, please contact:
+          <Ul>
+            <Li>
+              Nicolai Grymer: <Link href="mailto:ngry@itu.dk">ngry@itu.dk</Link>
+            </Li>
+            <Li>
+              Frederik Rothe: <Link href="mailto:frot@itu.dk">frot@itu.dk</Link>
+            </Li>
+          </Ul>
+        </Text>
 
         {/* Consent */}
-        
-          <Header variant="h2">Consent</Header>
-          <Text>
-            By installing and using this extension, you confirm that you have
-            read, understood, and agree to these Terms of Service. Your
-            continued usage constitutes your consent to be part of the research
-            under the outlined terms.
-          </Text>
-        
+
+        <Header variant="h2">Consent</Header>
+        <Text>
+          By installing and using this extension, you confirm that you have
+          read, understood, and agree to these Terms of Service. Your continued
+          usage constitutes your consent to be part of the research under the
+          outlined terms.
+        </Text>
 
         {/* Thank You */}
-        <Text>
-          Thank you for participating in our research.
-        </Text>
+        <Text>Thank you for participating in our research.</Text>
 
         <br />
         <br />
@@ -246,8 +231,7 @@ export default function App() {
             IT-University of Copenhagen
           </span>
         </Text>
-        
       </div>
     </main>
-  )
+  );
 }
