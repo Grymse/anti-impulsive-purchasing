a652045ef2f059f1478780f117076bd84167c187
Frederik Rothe <fr@notiva.dk>
2025-02-27 11:03:23 +0100

    fixed service worker not activating on deploy

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,11 +1,18 @@
-import { getUserId, sendAnalytics } from "~lib/analytics"
+import { getUserId } from "~lib/analytics"
 
+// This ensures the service worker activates properly
+console.log("Background service worker starting")
 
+// Set uninstall URL when extension starts
 getUserId().then((id) => {
     chrome.runtime.setUninstallURL(`https://www.lessextension.com/goodbye?userid=${id}`, () => {
-    }
-    )
-}
-)
+        console.log("Uninstall URL set successfully")
+    })
+})
+
+// Add a listener to make sure the service worker stays active
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension installed")
+})
 
 export {}
