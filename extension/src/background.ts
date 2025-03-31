366964c4610e5d3c2510adb6af2f4c1a450aceba
Frederik Rothe <fr@notiva.dk>
2025-03-31 10:45:23 +0200

    onupdate onboarding bug fixed

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,18 +1,26 @@
 import { getUserId } from "~lib/analytics"
+import { settings } from "~lib/settings"
 
 // Set uninstall URL when extension starts
-if (process.env.NODE_ENV !== "development") {
+if (true) {
     getUserId().then((id) => {
         chrome.runtime.setUninstallURL(`https://www.lessextension.com/goodbye?userid=${id}`, () => {
             console.log("Uninstall URL set successfully")
         })
     })
-    
-    chrome.runtime.onInstalled.addListener(() => {
-        chrome.tabs.create({ url: "https://www.lessextension.com/onboarding" }, () => {
-            console.log("Onboarding tab opened")
-        })
-    })
+
 }
+if (settings.getFromStorage().then(value => {
+    if (!value.hasSeenWelcomeModal) {
+        chrome.runtime.onInstalled.addListener(() => {
+            chrome.tabs.create({ url: "https://www.lessextension.com/onboarding" }, () => {
+                console.log("Onboarding tab opened")
+            })
+        })
+        }
+})) {
+    
+}
+
 
 export {}
